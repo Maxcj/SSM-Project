bab11a2d03787f6a0ae2c01451a22a52cfc74707
Maxcj <903283542@qq.com>
2020-08-02 13:32:16 +0800

    init project from https://ruoyi.vip/

---
+++ maxcj-admin/src/main/java/cn/maxcj/web/controller/system/SysRegisterController.java
@@ -0,0 +1,42 @@
+package cn.maxcj.web.controller.system;
+
+import cn.maxcj.common.core.controller.BaseController;
+import cn.maxcj.common.core.domain.AjaxResult;
+import cn.maxcj.framework.shiro.service.SysRegisterService;
+import cn.maxcj.system.domain.SysUser;
+import cn.maxcj.system.service.ISysConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 注册验证
+ *
+ * @author ruoyi
+ */
+@Controller
+public class SysRegisterController extends BaseController {
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    @ResponseBody
+    public AjaxResult ajaxRegister(SysUser user) {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser")))) {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ maxcj-framework/src/main/java/cn/maxcj/framework/shiro/service/SysLoginService.java
@@ -0,0 +1,116 @@
+package cn.maxcj.framework.shiro.service;
+
+import cn.maxcj.common.constant.Constants;
+import cn.maxcj.common.constant.ShiroConstants;
+import cn.maxcj.common.constant.UserConstants;
+import cn.maxcj.common.enums.UserStatus;
+import cn.maxcj.common.exception.user.*;
+import cn.maxcj.common.utils.DateUtils;
+import cn.maxcj.common.utils.MessageUtils;
+import cn.maxcj.common.utils.ServletUtils;
+import cn.maxcj.framework.manager.AsyncManager;
+import cn.maxcj.framework.manager.factory.AsyncFactory;
+import cn.maxcj.framework.util.ShiroUtils;
+import cn.maxcj.system.domain.SysUser;
+import cn.maxcj.system.service.ISysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * 登录校验方法
+ *
+ * @author ruoyi
+ */
+@Component
+public class SysLoginService {
+    @Autowired
+    private SysPasswordService passwordService;
+
+    @Autowired
+    private ISysUserService userService;
+
+    /**
+     * 登录
+     */
+    public SysUser login(String username, String password) {
+        // 验证码校验
+        if (!StringUtils.isEmpty(ServletUtils.getRequest().getAttribute(ShiroConstants.CURRENT_CAPTCHA))) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.error")));
+            throw new CaptchaException();
+        }
+        // 用户名或密码为空 错误
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("not.null")));
+            throw new UserNotExistsException();
+        }
+        // 密码如果不在指定范围内 错误
+        if (password.length() < UserConstants.PASSWORD_MIN_LENGTH
+                || password.length() > UserConstants.PASSWORD_MAX_LENGTH) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.not.match")));
+            throw new UserPasswordNotMatchException();
+        }
+
+        // 用户名不在指定范围内 错误
+        if (username.length() < UserConstants.USERNAME_MIN_LENGTH
+                || username.length() > UserConstants.USERNAME_MAX_LENGTH) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.not.match")));
+            throw new UserPasswordNotMatchException();
+        }
+
+        // 查询用户信息
+        SysUser user = userService.selectUserByLoginName(username);
+
+        if (user == null && maybeMobilePhoneNumber(username)) {
+            user = userService.selectUserByPhoneNumber(username);
+        }
+
+        if (user == null && maybeEmail(username)) {
+            user = userService.selectUserByEmail(username);
+        }
+
+        if (user == null) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.not.exists")));
+            throw new UserNotExistsException();
+        }
+
+        if (UserStatus.DELETED.getCode().equals(user.getDelFlag())) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.password.delete")));
+            throw new UserDeleteException();
+        }
+
+        if (UserStatus.DISABLE.getCode().equals(user.getStatus())) {
+            AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_FAIL, MessageUtils.message("user.blocked", user.getRemark())));
+            throw new UserBlockedException();
+        }
+
+        passwordService.validate(user, password);
+
+        AsyncManager.me().execute(AsyncFactory.recordLogininfor(username, Constants.LOGIN_SUCCESS, MessageUtils.message("user.login.success")));
+        recordLoginInfo(user);
+        return user;
+    }
+
+    private boolean maybeEmail(String username) {
+        if (!username.matches(UserConstants.EMAIL_PATTERN)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean maybeMobilePhoneNumber(String username) {
+        if (!username.matches(UserConstants.MOBILE_PHONE_NUMBER_PATTERN)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 记录登录信息
+     */
+    public void recordLoginInfo(SysUser user) {
+        user.setLoginIp(ShiroUtils.getIp());
+        user.setLoginDate(DateUtils.getNowDate());
+        userService.updateUserInfo(user);
+    }
+}
